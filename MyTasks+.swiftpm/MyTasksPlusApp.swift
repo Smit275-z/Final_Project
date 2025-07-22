5d8d548166539c34269ce178719d5b318adad2ba
Smit275-z <w0849357@myscc.ca>
2025-07-22 19:19:08 -0300

    File structure implemented

--- MyTasks+.swiftpm/MyTasksPlusApp.swift
+++ MyTasks+.swiftpm/MyTasksPlusApp.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 @main
-struct MyApp: App {
+struct MyTaskPlusApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TaskListView()
         }
     }
 }
---
+++ MyTasks+.swiftpm/Views/TaskListView.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct TaskListView: View {
+    var body: some View {
+        Text("Hello from TaskListView!")
+    }
+}
